34069074a93ae61153e430f02ae73866997918a3
sudhanshu singh chauhan <mininmalcrap@gmail.com>
2018-11-02 17:56:49 +0530

    feat(node-gigya): modify index.js to add api call test code.

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-module.exports = require ('./lib/Gigya');
+"use strict";
+module.exports = require('./lib/Gigya');
+
+const Gigya = require('./lib/Gigya');
+
+let main = () => {
+    const apiKey = '';
+    const secret = '';
+    const userKey = '';
+    let gigyaInstance = new Gigya(
+        apiKey,
+        secret
+    );
+    let prom = gigyaInstance.request(
+        'accounts.initRegistration',
+        { userKey: userKey, isLite: false });
+    prom.then(
+        (gigyaResponse) => {
+            console.log(gigyaResponse);
+            let regParams = {
+                email: 'tn2134@gmail.com',
+                password: 'Mordor121',
+                regToken: gigyaResponse['regToken'],
+                finalizeRegistration: true,
+                userKey: userKey
+            };
+            let promReg = gigyaInstance.request(
+                'accounts.register',
+                regParams
+                );
+            promReg.then(
+                (gigyaResponse) => {
+                    console.log(gigyaResponse);
+                },
+                (reason) => {
+                    console.log(reason);
+                }
+            );
+        },
+        (reason) => {
+            console.log(reason);
+        }
+    );
+
+    let loginParams = {
+        loginID: 'tn2134@gmail.com',
+        password: 'Mordor121',
+        userKey: userKey
+    }
+
+    let loginPromise = gigyaInstance.request(
+        'accounts.login',
+        loginParams
+    );
+    loginPromise.then(
+        (res) => {
+            console.log('login success!');
+            console.login(res);
+        },
+        (reason) => {
+            console.log('login failed');
+            console.log(reason);
+        }
+    );
+
+    let jwtParams = {
+        targetUID: '',
+        userKey: userKey
+    }
+    let jwtPromise = gigyaInstance.request(
+        'accounts.getJWT',
+        jwtParams
+    );
+
+    jwtPromise.then(
+        (respo) => {
+            console.log(respo);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+}
+
+main();
